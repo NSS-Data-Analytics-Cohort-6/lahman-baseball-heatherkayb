9d3214880db9f1d5214e57eebe2feedc28dc6c0c
heatherkayb <heatherkaym@gmail.com>
2022-03-05 13:58:06 -0600

    Finished 1-6, working on last part of 7

--- scripts/script1.sql
+++ scripts/script1.sql
@@ -46,7 +46,8 @@ GROUP BY position
 ORDER BY SUM(po) DESC;*/ 
 
 /*5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?*/
-/*SELECT ROUND(AVG(so), 2),
+/*
+SELECT ROUND(AVG(so), 2),
 		ROUND(AVG(hr), 2),
 CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
 	WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
@@ -62,20 +63,42 @@ CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
 FROM batting
 WHERE yearid >= 1920
 GROUP BY decade
-ORDER BY decade;*/
+ORDER BY decade;
+*/
 
-/*6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.*/
-
-SELECT b.playerid, p.namegiven, b.SB, b.CS, (b.SB+b.CS) AS total_attempts, b.SB/(b.SB+b.CS) AS successes 
+/*6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases. ANSWER: Christ Owings */
+/*
+SELECT b.playerid, p.namefirst, p.namelast, (b.SB+b.CS) AS total_attempts, ROUND(SUM(b.SB)*100.0 / NULLIF(SUM(b.SB+b.CS), 0), 2) AS percent_success 
 FROM batting AS b
 JOIN people AS p
 ON p.playerid = b.playerid
-WHERE (SB+CS) >=20;
+WHERE (SB+CS) >=20 AND yearid=2016
+GROUP BY b.playerid, p.namefirst, p.namelast, total_attempts
+ORDER BY percent_success DESC;
+*/
 
+/*7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 114 wins */
+/*SELECT teamid, yearid, wswin, SUM(w) AS total_wins
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016 AND wswin='Y'
+GROUP BY teamid, yearid, wswin
+ORDER BY total_wins DESC;*/
+/*What is the smallest number of wins for a team that did win the world series? 63 wins */ 
+/*SELECT teamid, yearid, wswin, SUM(w) AS total_wins
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016 AND wswin='Y'
+GROUP BY teamid, yearid, wswin
+ORDER BY total_wins;*/
 
-/*7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+/*Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?*/
 
-8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+SELECT teamid, yearid, wswin, SUM(w) AS total_wins
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016 AND wswin='Y'
+GROUP BY teamid, yearid, wswin
+ORDER BY total_wins DESC;
+
+/*8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
 
