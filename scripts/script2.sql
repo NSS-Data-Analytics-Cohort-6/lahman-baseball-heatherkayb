d06d7e817647e3a145929b73ea5c68a7630c581f
heatherkayb <heatherkaym@gmail.com>
2022-03-10 21:29:09 -0600

    Still stuck on 7c, 9, 10 but made progress

--- scripts/script2.sql
+++ scripts/script2.sql
@@ -1,11 +1,12 @@
-/*1. What range of years for baseball games played does the provided database cover? 1871-2016.*/ 
-SELECT *
-FROM homegames;
+/*1. What range of years for baseball games played does the provided database cover? 1871-2017.*/ 
+SELECT MIN(debut), MAX(finalgame)
+FROM people;
 
 /*2. Find the name and height of the shortest player in the database. Eddie Gaedel, aka Edward Carl, 43 inches.*/ 
 SELECT playerid, namefirst, namelast, namegiven, height
 FROM people
-ORDER BY height;
+ORDER BY height
+LIMIT 1;
 
 /*How many games did he play in? 1 */
 SELECT *
@@ -48,8 +49,8 @@ GROUP BY position
 ORDER BY SUM(po) DESC;
 
 /*5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?*/
-SELECT ROUND(AVG(so), 2) AS avg_strikeouts,
-		ROUND(AVG(hr), 2) AS avg_homeruns,
+SELECT ROUND(AVG(so /g), 2) AS avg_strikeouts,
+		ROUND(AVG(hr / g), 2) AS avg_homeruns,
 CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
 	WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
 	WHEN yearid BETWEEN 1940 AND 1949 THEN '1940s'
@@ -76,10 +77,10 @@ GROUP BY b.playerid, p.namefirst, p.namelast, total_attempts
 ORDER BY percent_success DESC;
 
 
-/*7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 114 wins */
+/*7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 116 wins */
 SELECT teamid, yearid, wswin, SUM(w) AS total_wins
 FROM teams
-WHERE yearid BETWEEN 1970 AND 2016 AND wswin='Y'
+WHERE yearid BETWEEN 1970 AND 2017 AND wswin='N'
 GROUP BY teamid, yearid, wswin
 ORDER BY total_wins DESC;
 
@@ -91,7 +92,7 @@ GROUP BY teamid, yearid, wswin
 ORDER BY total_wins;
 
 /*Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?*/
-SELECT subquery.teamid, subquery.yearid, MAX(total_wins)
+/*SELECT subquery.teamid, subquery.yearid, MAX(total_wins)
 FROM 
 	(SELECT teamid, yearid, wswin, SUM(w) AS total_wins
 	FROM teams
@@ -100,7 +101,7 @@ FROM
 	ORDER BY total_wins DESC, yearid) AS subquery
 WHERE wswin='Y', MAX(total_wins)=subquery.total_wins
 GROUP BY subquery.yearid, subquery.teamid, subquery.wswin
-ORDER BY max DESC;
+ORDER BY max DESC;*/
 
 SELECT teamid, yearid, wswin, SUM(w) AS total_wins
 	FROM teams
@@ -124,22 +125,102 @@ ORDER BY avg_attendance
 LIMIT 5;
 
 /*9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.*/
-SELECT COUNT(a.awardid),
+ 
+/*SELECT *
+FROM managers
+ORDER BY playerid
+
+SELECT teamid, yearid, playerid
+FROM managers
+WHERE yearid IN (1988, 1990, 1992, 1997, 2006, 2012)
+	AND playerid IN ('leylaji99', 'johnsda02')
+ORDER BY playerid
+
+LEFT JOIN people AS p
+USING (teamid)
+WHERE p.playerid IN ('leylaji99', 'johnsda02')*/
+
+/*WITH n AS (
+SELECT a.awardid,
 	p.namefirst,
 	p.namelast,
+	a.yearid,
+	a.playerid,
 	a.lgid
 FROM awardsmanagers AS a
 INNER JOIN people AS p
-ON a.playerid = p.playerid
-WHERE a.awardid = 'TSN Manager of the Year'
+USING (playerid)
+WHERE awardid = 'TSN Manager of the Year'
 	AND a.lgid = 'NL'
-GROUP BY p.namelast, p.namefirst, a.lgid
-ORDER BY p.namelast DESC, p.namefirst;
+UNION 
+SELECT a.awardid,
+	p.namefirst,
+	p.namelast,
+	a.yearid,
+	a.playerid,
+	a.lgid
+FROM awardsmanagers AS a
+INNER JOIN people AS p
+USING (playerid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND a.lgid = 'AL')
+	
+SELECT n.namelast,
+	n.namefirst,
+	t.name
+FROM n
+INNER JOIN a
+ON a.playerid = n.playerid
+LEFT JOIN people AS p
+ON a.playerid = p.playerid
+LEFT JOIN teams AS t
+ON t.lgid = n.lgid;*/
 
-2 CTES JOINED TOGETHER IN A WAY THAT INVOLVES ONLY GUYS THAT ARE IN BOTH
-INNER JOIN 
 
-WITH n AS (
+/*WITH n AS (
+	SELECT a.awardid,
+	p.namefirst,
+	p.namelast,
+	a.yearid,
+	a.playerid,
+	a.lgid
+FROM awardsmanagers AS a
+INNER JOIN people AS p
+USING (playerid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND a.lgid = 'NL'),
+	
+	a AS (
+	SELECT a.awardid,
+	p.namefirst,
+	p.namelast,
+	a.yearid,
+	a.playerid,
+	a.lgid
+FROM awardsmanagers AS a
+INNER JOIN people AS p
+USING (playerid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND a.lgid = 'AL')
+	
+SELECT n.namelast,
+	n.namefirst,
+	n.lgid,
+	a.lgid,
+	n.yearid AS nl_year, 
+	a.yearid as al_year,
+	t.name
+FROM n
+INNER JOIN a
+ON a.playerid = n.playerid
+LEFT JOIN people AS p
+ON a.playerid = p.playerid
+LEFT JOIN teams AS t
+ON t.lgid = n.lgid;*/
+
+
+
+/*WITH n AS (
 	SELECT a.awardid,
 	p.namefirst,
 	p.namelast,
@@ -161,7 +242,7 @@ WHERE awardid = 'TSN Manager of the Year'
 	a.lgid
 FROM awardsmanagers AS a
 INNER JOIN people AS p
-ON a.playerid = p.playerid
+USING (playerid)
 WHERE awardid = 'TSN Manager of the Year'
 	AND a.lgid = 'AL')
 	
@@ -170,17 +251,28 @@ SELECT n.namelast,
 	n.lgid,
 	a.lgid,
 	n.yearid AS nl_year, 
-	a.yearid as al_year	
+	a.yearid as al_year
 FROM n
 INNER JOIN a
-ON a.playerid = n.playerid
+USING(playerid)
 LEFT JOIN people AS p
-ON a.playerid = p.playerid;
+USING(playerid);*/
 
 /*10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.*/
-SELECT p.playerid, SUM(p.HR) AS total_homeruns, DATE_PART('year', CAST(l.debut AS date)) AS decade
-FROM pitchingpost AS p
+/*SELECT b.playerid, SUM(b.HR) AS total_homeruns, b.yearid
+FROM battingpost AS b
 LEFT JOIN people AS l
-ON p.playerid = l.playerid
-WHERE DATE_PART('year', CAST(l.debut AS date)) < 2006
-GROUP BY p.playerid, l.debut;
+USING (playerid)
+WHERE yearid > 2000
+GROUP BY b.playerid, l.debut, b.yearid
+ORDER BY total_homeruns DESC;*/
+
+SELECT p.namelast,
+	p.namefirst,
+	MAX(b.hr),
+	COUNT(b.yearid) AS seasons_played
+FROM people AS p
+LEFT JOIN batting AS b
+USING (playerid)
+GROUP BY p.namelast, p.namefirst
+ORDER BY p.namelast, p.namefirst;
